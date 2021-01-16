059b352b107a04d55aae874de107d06967ae27e9
shohruhmalikov <77524704+shohruhmalikov@users.noreply.github.com>
2021-01-16 14:37:47 +0500

    Update string.php

--- string.php
+++ string.php
@@ -1,1 +1,13 @@
+1. 11 dan 40 gacha bo'lgan sonlar orasidan juft sonlarini ajratib olamiz.
+va continue funksiyasi orqali sikl o'tqazib yuboramiz
+
+for ($i=11; $i < 40; $i++) { 
+	if ($i%2 == 1) {
+		continue;
+	} else {
+		echo $i.'<br>';
+	}
+}
+
+                    
 
